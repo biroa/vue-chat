489745491d05e20258ed298d252133a7687612e5
biroa <adam.biro@gmail.com>
2017-06-27 11:00:13 +0100

    Init Vue things to Laravel env.

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+	<div>
+		<p>{{ message }}</p>
+		<small>{{ name }}</small>
+	</div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                message: "test",
+	            name: "Johny Bravo"
+            }
+        }
+    }
+</script>
+
+<style lang="css">
+
+</style>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Charroom</title>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <div id="app">
+            <h1>Chatroom</h1>
+            <chat-message></chat-message>
+            {{--<chat-log></chat-log>--}}
+            {{--<chat-composer></chat-composer>--}}
+        </div>
+
+        <script src="js/app.js" charset="utf-8"></script>
+    </body>
+
+</html>
