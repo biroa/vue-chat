ec7937e277f5576dd215885f7f30060e55a7c7d8
biroa <adam.biro@gmail.com>
2017-06-27 15:51:26 +0100

    set migration, extend relations

--- app/Message.php
+++ app/Message.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+
+    protected $fillable = ['message'];
+
+    /**
+     * @return object
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
